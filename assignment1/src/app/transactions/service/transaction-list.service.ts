be1aa5ce5b9cff9c795e8aff1362b04edcab28a6
Sylvesterronn <sylvesterronn01@gmail.com>
2025-09-09 08:53:17 +0200

    implement service and transaction component

---
+++ assignment1/src/app/transactions/service/transaction-list.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Router} from 'express';
+import {AuthService} from '../../login/service/login.service';
+import {CreditCard} from '../../credit-card/service/credit-card.service';
+import {firstValueFrom} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TransactionListService {
+
+  constructor(private http: HttpClient, private authService: AuthService) { }
+
+  getTransactionList():Promise<Transaction[]>
+  {
+    const token = this.authService.getToken();
+    return firstValueFrom(
+      this.http.get<Transaction[]>('https://assignment1.swafe.dk/api/Transaction',
+      {
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      }
+      )
+    );
+  }
+}
+
+export interface Transaction {
+  cardNumber:number;
+  amount:number;
+  currency:string;
+  comment:string;
+  date:Date;
+}
--- assignment1/src/app/transactions/transactions.component.ts
+++ assignment1/src/app/transactions/transactions.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, inject, Inject, signal} from '@angular/core';
+import {Transaction, TransactionListService} from './service/transaction-list.service';
+import {error} from '@angular/compiler-cli/src/transformers/util';
 
 @Component({
-    selector: 'app-transactions',
-    imports: [],
-    templateUrl: './transactions.component.html',
-    styleUrl: './transactions.component.css'
+  selector: 'app-transactions',
+  imports: [],
+  templateUrl: './transactions.component.html',
+  standalone: true,
+  styleUrl: './transactions.component.css'
 })
 export class TransactionsComponent {
+  transactionsListService = inject(TransactionListService);
+  transaction= signal<Transaction[]>([]);
 
+  constructor() {
+    this.loadTransaction();
+  }
+
+
+  private async loadTransaction() {
+    try{
+      const data = await this.transactionsListService.getTransactionList();
+      this.transaction.set(data);
+    }
+    catch (error) {
+      console.error("Error loading transaction:", error);
+    }
+  }
 }
