18d39383e14370611c0c138779595e0a75e028b7
Hiiks <nikolaj.dahl@outlook.com>
2025-09-22 09:14:58 +0200

    add: remove and add transaction to transaction page

--- assignment1/src/app/transactions/service/transaction-list.service.ts
+++ assignment1/src/app/transactions/service/transaction-list.service.ts
@@ -1,35 +1,64 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Router} from 'express';
-import {AuthService} from '../../login/service/login.service';
-import {firstValueFrom} from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Router } from 'express';
+import { AuthService } from '../../login/service/login.service';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TransactionListService {
+  constructor(private http: HttpClient, private authService: AuthService) {}
 
-  constructor(private http: HttpClient, private authService: AuthService) { }
-
-  getTransactionList():Promise<Transaction[]>
-  {
+  getTransactionList(): Promise<Transaction[]> {
     const token = this.authService.getToken();
     return firstValueFrom(
-      this.http.get<Transaction[]>('https://assignment1.swafe.dk/api/Transaction',
-      {
-        headers: {
-          authorization: `Bearer ${token}`,
-        },
-      }
+      this.http.get<Transaction[]>(
+        'https://assignment1.swafe.dk/api/Transaction',
+        {
+          headers: {
+            authorization: `Bearer ${token}`,
+          },
+        }
+      )
+    );
+  }
+
+  addTransaction(transaction: Transaction): Promise<Transaction> {
+    const token = this.authService.getToken();
+    return firstValueFrom(
+      this.http.post<Transaction>(
+        'https://assignment1.swafe.dk/api/Transaction',
+        transaction,
+        {
+          headers: {
+            authorization: `Bearer ${token}`,
+          },
+        }
+      )
+    );
+  }
+
+  deleteTransaction(uid: number): Promise<void> {
+    const token = this.authService.getToken();
+    return firstValueFrom(
+      this.http.delete<void>(
+        `https://assignment1.swafe.dk/api/Transaction/uid?uid=${uid}`,
+        {
+          headers: {
+            authorization: `Bearer ${token}`,
+          },
+        }
       )
     );
   }
 }
 
 export interface Transaction {
-  cardNumber:number;
-  amount:number;
-  currency:string;
-  comment:string;
-  transactionDate:Date;
+  uid?: number;
+  cardNumber: number;
+  amount: number;
+  currencyCode: string;
+  comment: string;
+  transactionDate: Date;
 }
